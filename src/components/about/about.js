0e007ccb224f03acdd324993ee6f767b3fa7e794
poldeeek <pawelp258@gmail.com>
2021-05-11 04:20:03 +0200

    start working on about component

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,12 +1,53 @@
+import { useStaticQuery, graphql } from "gatsby"; // to query for image data
 import React from "react";
+import Img from "gatsby-image";
 
 import "./about.scss";
 
 const About = () => {
+  const [hasMounted, setHasMounted] = React.useState(false);
+
+  const data = useStaticQuery(graphql`
+    query Photo {
+      file(relativePath: { eq: "avatar.jpeg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+
+  React.useEffect(() => {
+    setHasMounted(true);
+  }, []);
+  if (!hasMounted) {
+    return null;
+  }
+
+  console.log(data);
+
   return (
     <div className="about">
-      <div>Photo</div>
-      <div>Text about me</div>
+      <div className="about__profile">
+        <Img
+          fluid={data.file.childImageSharp.fluid}
+          className="about__profile__photo"
+        />
+      </div>
+      <section>
+        <h2>O mnie</h2>
+        <article>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </article>
+      </section>
     </div>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,12 @@ import * as React from "react";
 import Layout from "../components/layout/layout";
 import About from "../components/about/about";
 
-const IndexPage = () => (
-  <Layout>
-    <About />
-  </Layout>
-);
+const IndexPage = ({ data }) => {
+  return (
+    <Layout>
+      <About />
+    </Layout>
+  );
+};
 
 export default IndexPage;
