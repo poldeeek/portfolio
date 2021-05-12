748c2d4fc64949ebe90e94cc156a3a49e3c9e161
poldeeek <pawelp258@gmail.com>
2021-05-12 02:30:09 +0200

    css hover animations

--- src/data/jobs.js
+++ src/data/jobs.js
@@ -2,6 +2,7 @@ const jobs = [
   {
     name: "Software Engineer",
     company: "Photocrowd Ltd",
+    technologies: ["React.js", "Less", "Django"],
     duration: "grudzień 2020 - marzec 2021",
   },
 ];
