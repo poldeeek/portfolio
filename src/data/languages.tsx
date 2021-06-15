6b2e308bc9e62a6d31d2801ee5a00a7d4464463f
poldeeek <pawelp258@gmail.com>
2021-06-15 23:39:03 +0200

    add typescript to languages.tsx

---
+++ src/data/languages.tsx
@@ -0,0 +1,16 @@
+// add new language as first in array
+
+type ILanguages = Array<{ name: string; level: string }>;
+
+const languages: ILanguages = [
+  {
+    name: 'Angielski',
+    level: 'B2'
+  },
+  {
+    name: 'Polski',
+    level: 'C2'
+  }
+];
+
+export default languages;
