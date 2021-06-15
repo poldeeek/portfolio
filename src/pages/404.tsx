cf7152925209475ca7c42960d27c11bb6939b8ac
poldeeek <pawelp258@gmail.com>
2021-06-16 00:51:00 +0200

    added typescript to 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,11 +3,11 @@ import * as React from 'react';
 import Layout from '../components/layout/layout';
 import Seo from '../components/seo';
 
-const NotFoundPage = () => (
-    <Layout>
-        <Seo title='404: Not found' />
-        <h1>Podana strona nie istnieje.</h1>
-    </Layout>
+const NotFoundPage: React.FC = () => (
+  <Layout>
+    <Seo lang={'pl-PL'} title='404: Not found' />
+    <h1>Podana strona nie istnieje.</h1>
+  </Layout>
 );
 
 export default NotFoundPage;
